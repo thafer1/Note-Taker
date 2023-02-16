6fd9da7083480f61d93442ba9d74d44b2f5b8359
thafer1 <112597461+ZedZappie@users.noreply.github.com>
2023-02-16 18:21:27 -0500

    Connected routes to server.js

--- routes/html.js
+++ routes/html.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 // returns note.html file
 router.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
 // returns index.html file
 router.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
   module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // adds dependencies
 const express = require('express');
-const html = require('./routes/html.js');
+const routes = require('./routes/html.js');
 
 // builds express app
 const app = express();
 
 // builds a port
 const PORT = process.env.PORT || 3000;
+
+app.use(routes);
 
 // starts server using port
 app.listen(PORT, function () {console.log(`Listening on PORT: ${PORT}`)});
